c8bc2ef93fd2acd30155fa9e17d75ec859ac2cbe
aghaseyed1 <aghaseyed7@gmail.com>
2022-09-25 22:41:15 +0330

    first:)

---
+++ test-cf.py
@@ -0,0 +1,24 @@
+import operator
+import requests
+s=requests.Session()
+ips = []
+d = {}
+with open("ips.txt","r") as f:
+   for i in f:
+       ips.append(i.strip())
+
+for ip in ips:
+    try:
+        r=s.get("http://"+ip+"/ray", headers={"Host":"localhoster.ml"},timeout=0.4)
+    except:
+        pass
+    print(r,r.elapsed.total_seconds())
+    if r.status_code==400:
+        d.update({ip:int(r.elapsed.total_seconds()*1000)})
+        with open("tested-ip.txt",'a') as f:
+            f.write(f"{ip},{int(r.elapsed.total_seconds()*1000)}\n")
+
+with open("test-ip.txt",'w') as f:
+    for i in sorted(d.items(), key=operator.itemgetter(1)):
+        print(f"{i[0]},{i[1]}ms")
+        f.write(f"{i[0]},{i[1]}ms\n")
