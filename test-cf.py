99e93bd529bf3dfa81167796a20711126f387897
aghaseyed1 <aghaseyed7@gmail.com>
2022-09-28 18:36:07 +0330

    ips shuffle

--- test-cf.py
+++ test-cf.py
@@ -2,10 +2,15 @@ import os
 import sys
 import operator
 import requests
+import random
 
-timeout=1
+timeout = 1
+ips = []
 ok = "\033[1;32mok\033[0m"
 bad = "\033[1;31mbad\033[0m"
+filtered = "\033[1;35mfiltered\033[0m"
+bad_ping = "\033[1;35mbad ping\033[0m"
+
 def test_ip():
     session = requests.Session()
     ip_dict = {}
@@ -13,28 +18,28 @@ def test_ip():
     with open("ips.txt", "r") as ip_file:
 
         for ip in ip_file.readlines():
-            ip = ip.strip()
+            ips.append(ip.strip())
+    random.shuffle(ips)
+    for ip in ips:
+        try:
+            result = session.get(
+                f"http://{ip}/ray", headers={"Host": "localhoster.ml"}, timeout=timeout)
 
-            try:
-                result = session.get(
-                    f"http://{ip}/ray", headers={"Host": "localhoster.ml"}, timeout=timeout)
+            print(
+                f"{ip}, Status: {ok if result.status_code == 400 else bad}, Elapsed-Time: {result.elapsed.total_seconds()}")
 
-                print(
-                    f"{ip}, Status: {ok if result.status_code == 400 else bad}, Elapsed-Time: {result.elapsed.total_seconds()}")
-
-                if result.status_code == 400:
-                    ip_dict.update({ip: int(result.elapsed.total_seconds()*1000)})
-                    with open("tested-ip.txt", 'a') as f:
-                        f.write(
-                            f"{ip} Latency: {int(result.elapsed.total_seconds()*1000)}\n")
-            # Interrupt by User
-            except KeyboardInterrupt:
-                print("Interrupted!")
-                sys.exit()
-            # Any other Error Will be Ignored
-            except:
-                print(f"IP {ip} filtered or bad ping!")
-
+            if result.status_code == 400:
+                ip_dict.update({ip: int(result.elapsed.total_seconds()*1000)})
+                with open("tested-ip.txt", 'a') as f:
+                    f.write(
+                        f"{ip} Latency: {int(result.elapsed.total_seconds()*1000)}\n")
+        # Interrupt by User
+        except KeyboardInterrupt:
+            print("Interrupted!")
+            sys.exit()
+        # Any other Error Will be Ignored
+        except:
+            print(f"IP {ip} {filtered} or {bad_ping}!")
     return ip_dict
 
 
@@ -45,10 +50,12 @@ def Sort_late_time():
         for i in sorted(d.items(), key=operator.itemgetter(1)):
             sorted_file.write(f"{i[0]} -- Latency:{i[1]}ms\n")
 
+
 def The_Best_Ips():
     print("\nThe 10 Best IP (Lower Latency)\n")
     os.system("bash -c 'head Sorted-IP.txt'")
     print()
+
 
 Sort_late_time()
 The_Best_Ips()
